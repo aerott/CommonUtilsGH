b823cbc42a15fcd544b6e2f6f926d39838d738c0
azure.admin <azure@shockit.pl>
2020-12-16 09:54:04 +0100

    added initial commit

---
+++ CommonUtilsGH/CommonUtilsGH/CommonModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonUtils
+{
+    public class CommonModule
+    {
+        public void DoOperation()
+        {
+            Console.WriteLine("Called DoOperation");
+        }
+    }
+}
